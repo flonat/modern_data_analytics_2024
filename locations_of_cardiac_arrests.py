6ecb6f5ea20f11c7a1262e48ed6c1b051db668b4
yanndebelgique <yann.buydens@gmail.com>
2024-03-30 14:22:04 +0100

    deleted file

--- locations_of_cardiac_arrests.py
+++
@@ -1,9 +0,0 @@
-
-# file interventions_bxl2.parquet.gzip
-cardiac_arrest_event_types = [
-    'HARTSTILSTAND - DOOD - OVERLEDEN',
-    'PIJN OP DE BORST',
-    'CARDIAAL PROBLEEM (ANDERE DAN PIJN AAN DE BORST)'
-]
-
-# file interventions_bxl.parquet.gzip
